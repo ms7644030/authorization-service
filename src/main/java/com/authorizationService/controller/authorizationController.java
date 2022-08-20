fa63e2030525a2c45e4f2d4dcb29b05c8f3e19a6
MANISH <ms7644030@gmail.com>
2022-08-20 11:06:40 +0530

    pom.xml , Dockerfile modified

--- src/main/java/com/authorizationService/controller/authorizationController.java
+++ src/main/java/com/authorizationService/controller/authorizationController.java
@@ -1,13 +1,16 @@
 package com.authorizationService.controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +23,8 @@ import com.authorizationService.util.JwtUtil;
 @RestController
 //@CrossOrigin(origins="*")
 public class authorizationController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(authorizationController.class);
 	
 	@Autowired
 	private AuthenticationManager authenticationManager;
@@ -34,10 +39,14 @@ public class authorizationController {
 	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest ) throws Exception{
 		
 		try {
+			LOGGER.info("Received Request for user authentication, user : ", authenticationRequest.getUsername());
+			
 		authenticationManager.authenticate(
 				new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword()));
 		}
 		catch(BadCredentialsException e) {
+			
+			LOGGER.info("Autentication failed(Incorrect username or password), user : ", authenticationRequest.getUsername());
 			throw new Exception("Incorrect username or password" , e);
 		}
 		
